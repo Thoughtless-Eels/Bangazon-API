e46cf9b26fc9d320296d54a3646a8ff77cad493e
John Dulaney <john.s.dulaney1@gmail.com>
2018-01-24 15:47:12 -0600

    pre conflict merge

--- Startup.cs
+++ Startup.cs
@@ -11,38 +11,32 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using thoughtless_eels.Data;
 
-namespace thoughtless_eels
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace thoughtless_eels {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            
-            services.AddMvc();
-            var connection = "Filename=/Users/j/Workspace/group-projects/thoughtless-eels/Data/InitialDB.db";
-            Console.WriteLine($"connection = {connection}");
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connection));
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+
+            services.AddMvc ();
+            var connection = System.Environment.GetEnvironmentVariable ("EelDB");
+            Console.WriteLine ($"connection = {connection}");
+            services.AddDbContext<ApplicationDbContext> (options => options.UseSqlite (connection));
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-      
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
