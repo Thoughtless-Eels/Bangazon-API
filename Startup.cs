d43d018f6487fe4066d76e326cdae43c034e1af0
Courtney Seward <courtneyseward93@gmail.com>
2018-01-24 15:42:09 -0600

    successfully created migration files

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using thoughtless_eels.Data;
+
 
 namespace thoughtless_eels
 {
@@ -23,7 +26,13 @@ namespace thoughtless_eels
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+        {
             services.AddMvc();
+            var connection = "/Users/courtneyseward/workspace/csharp/Bangazon/thoughtless-eels/Data/DbContext.cs";
+            Console.WriteLine($"connection = {connection}");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connection));
+        }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
