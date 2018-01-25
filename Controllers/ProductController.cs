add8a3a653f2f13f8b3978132aab6a7c25c9f367
John Dulaney <john.s.dulaney1@gmail.com>
2018-01-25 10:08:50 -0600

    refactored product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using thoughtless_eels.Data;
+using thoughtless_eels.Models;
+
+namespace thoughtless_eels.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _context;
+        // Constructor method to create an instance of context to communicate with our database.
+        public ProductController(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = _context.Product.ToList();
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        // GET api/Product/5
+        [HttpGet("{id}", Name = "GetSingleProduct")]
+        public IActionResult Get(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Product product = _context.Product.Single(g => g.ProductId == id);
+
+                if (product == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(product);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Product.Add(product);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductExists(product.ProductId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtRoute("GetSingleProduct", new { id = product.ProductId }, product);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+            _context.Product.Update(product);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product product = _context.Product.Single(g => g.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Product.Remove(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+
+        private bool ProductExists(int productId)
+        {
+            return _context.Product.Any(g => g.ProductId == productId);
+        }
+
+    }
+}
