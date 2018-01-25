86aa368c277a483fff0ef5fcc066e83ff60715f4
John Dulaney <john.s.dulaney1@gmail.com>
2018-01-25 10:21:54 -0600

    formatting

--- Controllers/ProductType.cs
+++ Controllers/ProductType.cs
@@ -8,135 +8,104 @@ using Microsoft.EntityFrameworkCore;
 using thoughtless_eels.Data;
 using thoughtless_eels.Models;
 
-namespace thoughtless_eels.Controllers
-{
-    [Route("api/[controller]")]
-    public class ProductTypeTypeController : Controller
-    {
+namespace thoughtless_eels.Controllers {
+    [Route ("api/[controller]")]
+    public class ProductTypeTypeController : Controller {
         private ApplicationDbContext _context;
         // Constructor method to create an instance of context to communicate with our database.
-        public ProductTypeTypeController(ApplicationDbContext ctx)
-        {
+        public ProductTypeTypeController (ApplicationDbContext ctx) {
             _context = ctx;
         }
 
         [HttpGet]
-        public IActionResult Get()
-        {
-            var productTypes = _context.ProductType.ToList();
-            if (productTypes == null)
-            {
-                return NotFound();
+        public IActionResult Get () {
+            var productTypes = _context.ProductType.ToList ();
+            if (productTypes == null) {
+                return NotFound ();
             }
-            return Ok(productTypes);
+            return Ok (productTypes);
         }
 
         // GET api/productType/5
-        [HttpGet("{id}", Name = "GetSingleProductType")]
-        public IActionResult Get(int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
+        [HttpGet ("{id}", Name = "GetSingleProductType")]
+        public IActionResult Get (int id) {
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
             }
 
-            try
-            {
-                ProductType productType = _context.ProductType.Single(g => g.ProductTypeId == id);
+            try {
+                ProductType productType = _context.ProductType.Single (g => g.ProductTypeId == id);
 
-                if (productType == null)
-                {
-                    return NotFound();
+                if (productType == null) {
+                    return NotFound ();
                 }
 
-                return Ok(productType);
-            }
-            catch (System.InvalidOperationException ex)
-            {
-                return NotFound();
+                return Ok (productType);
+            } catch (System.InvalidOperationException ex) {
+                return NotFound ();
             }
         }
 
         // POST api/values
         [HttpPost]
-        public IActionResult Post([FromBody]ProductType productType)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
+        public IActionResult Post ([FromBody] ProductType productType) {
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
             }
 
-            _context.ProductType.Add(productType);
+            _context.ProductType.Add (productType);
 
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (ProductTypeExists(productType.ProductTypeId))
-                {
-                    return new StatusCodeResult(StatusCodes.Status409Conflict);
-                }
-                else
-                {
+            try {
+                _context.SaveChanges ();
+            } catch (DbUpdateException) {
+                if (ProductTypeExists (productType.ProductTypeId)) {
+                    return new StatusCodeResult (StatusCodes.Status409Conflict);
+                } else {
                     throw;
                 }
             }
-            return CreatedAtRoute("GetSingleProductType", new { id = productType.ProductTypeId }, productType);
+            return CreatedAtRoute ("GetSingleProductType", new { id = productType.ProductTypeId }, productType);
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]ProductType productType)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
+        [HttpPut ("{id}")]
+        public IActionResult Put (int id, [FromBody] ProductType productType) {
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
             }
 
-            if (id != productType.ProductTypeId)
-            {
-                return BadRequest();
+            if (id != productType.ProductTypeId) {
+                return BadRequest ();
             }
-            _context.ProductType.Update(productType);
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductTypeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
+            _context.ProductType.Update (productType);
+            try {
+                _context.SaveChanges ();
+            } catch (DbUpdateConcurrencyException) {
+                if (!ProductTypeExists (id)) {
+                    return NotFound ();
+                } else {
                     throw;
                 }
             }
 
-            return new StatusCodeResult(StatusCodes.Status204NoContent);
+            return new StatusCodeResult (StatusCodes.Status204NoContent);
         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            ProductType productType = _context.ProductType.Single(g => g.ProductTypeId == id);
+        [HttpDelete ("{id}")]
+        public IActionResult Delete (int id) {
+            ProductType productType = _context.ProductType.Single (g => g.ProductTypeId == id);
 
-            if (productType == null)
-            {
-                return NotFound();
+            if (productType == null) {
+                return NotFound ();
             }
-            _context.ProductType.Remove(productType);
-            _context.SaveChanges();
-            return Ok(productType);
+            _context.ProductType.Remove (productType);
+            _context.SaveChanges ();
+            return Ok (productType);
         }
 
-        private bool ProductTypeExists(int productTypeId)
-        {
-            return _context.ProductType.Any(g => g.ProductTypeId == productTypeId);
+        private bool ProductTypeExists (int productTypeId) {
+            return _context.ProductType.Any (g => g.ProductTypeId == productTypeId);
         }
 
     }
