fa6c44f3e196c2fe5ab7b74883499fa0a09c3059
Gward2489 <gww8804@gmail.com>
2018-01-30 13:46:02 -0600

    ajax testers

--- Startup.cs
+++ Startup.cs
@@ -39,8 +39,6 @@ namespace thoughtless_eels
 
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
